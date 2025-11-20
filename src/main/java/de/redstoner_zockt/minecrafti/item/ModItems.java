05ca0445224ae834b65115e6af154ad74947e078
luis <luisganz10@gmail.com>
2025-11-20 22:36:04 +0100

    add example item

--- src/main/java/de/redstoner_zockt/minecrafti/item/ModItems.java
+++ src/main/java/de/redstoner_zockt/minecrafti/item/ModItems.java
@@ -1,9 +1,7 @@
-package de.redstoner_zockt.nicemod.item;
+package de.redstoner_zockt.minecrafti.item;
 
-import de.redstoner_zockt.nicemod.Minecrafti;
-import net.minecraft.world.item.Item;
+import de.redstoner_zockt.minecrafti.Minecrafti;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
