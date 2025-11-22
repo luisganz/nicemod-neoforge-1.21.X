b3d71446b99b68f013ba2b6de302278f247a28ab
redstoner_zockt <luisganz10@gmail.com>
2025-11-22 22:07:46 +0100

    add knife

--- src/main/java/de/redstoner_zockt/minecraftplus/item/ModItems.java
+++ src/main/java/de/redstoner_zockt/minecraftplus/item/ModItems.java
@@ -1,7 +1,9 @@
 package de.redstoner_zockt.minecraftplus.item;
 
 import de.redstoner_zockt.minecraftplus.Minecraftplus;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
@@ -10,6 +12,9 @@ public class ModItems {
     //example
     //public static final DeferredItem<Item> (NAME) = ITEMS.register("(name)",
     //        () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item>KNIFE = ITEMS.register("knife",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
