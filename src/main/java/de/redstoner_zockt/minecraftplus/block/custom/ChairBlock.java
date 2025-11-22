56abb21441506021e911dce027fb6352580932bd
redstoner_zockt <luisganz10@gmail.com>
2025-11-22 21:33:51 +0100

    changed chair

--- src/main/java/de/redstoner_zockt/minecraftplus/block/custom/ChairBlock.java
+++ src/main/java/de/redstoner_zockt/minecraftplus/block/custom/ChairBlock.java
@@ -2,17 +2,20 @@ package de.redstoner_zockt.minecraftplus.block.custom;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
 
 public class ChairBlock extends HorizontalDirectionalBlock {
     public static final MapCodec<PlateBlock> CODEC = simpleCodec(PlateBlock::new);
-    private static final VoxelShape SHAPE = Block.box(3, 0, 4, 13, 16, 13);
+    private static final VoxelShape SHAPE = Block.box(3, 0, 3, 13, 16, 13);
 
     public ChairBlock(BlockBehaviour.Properties properties) {
         super(properties.noOcclusion());
@@ -26,5 +29,15 @@ public class ChairBlock extends HorizontalDirectionalBlock {
     @Override
     protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
