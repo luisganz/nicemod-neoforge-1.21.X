28078c1426181debb40abff85f288cf22efbe1ac
redstoner_zockt <luisganz10@gmail.com>
2026-02-27 15:49:44 +0100

    added deco

---
+++ src/main/java/de/redstoner_zockt/minecraftplus/block/custom/NightTableBlock.java
@@ -0,0 +1,36 @@
+package de.redstoner_zockt.minecraftplus.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+
+public class NightTableBlock extends HorizontalDirectionalBlock {
+    public static final MapCodec<PlateBlock> CODEC = simpleCodec(PlateBlock::new);
+
+    public NightTableBlock(Properties properties) {
+        super(properties.noOcclusion());
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
